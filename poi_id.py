a25094e7e27603d1e3a444cc59180715de1c04bf
Jennifer Cooper <tx.jsullivan@gmail.com>
2016-07-03 13:47:11 -0700

    Playing around with PCA

--- poi_id.py
+++ poi_id.py
@@ -11,7 +11,8 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary', 'deferral_payments', 'exercised_stock_options', 'bonus', 'restricted_stock', 'long_term_incentive', 'shared_receipt_with_poi', 'from_this_person_to_poi', 'from_poi_to_this_person']
+# You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -21,6 +22,7 @@ with open("final_project_dataset.pkl", "r") as data_file:
 data_dict.pop( "TOTAL", 0 )
 
 ### Task 3: Create new feature(s)
+
 
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -34,6 +36,7 @@ labels, features = targetFeatureSplit(data)
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
+
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
@@ -52,9 +55,14 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
     
-clf.fit(features_train, labels_train)
-pred = clf.predict(features_test)
-#print accuracy_score(labels_test, pred)
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+pca_features_train = pca.fit_transform(features_train)
+pca_features_test = pca.fit_transform(features_test)
+    
+clf.fit(pca_features_train, labels_train)
+pred = clf.predict(pca_features_test)
+print accuracy_score(labels_test, pred)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
